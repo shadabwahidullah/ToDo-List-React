ebf0cfeef69fd852bb15bb578facf97d85e92da1
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-02 23:32:44 +0430

    Make checkbox controlled input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ class TodoItem extends React.Component {
   render() {
     return (
       <li>
-        <input type="checkbox" />
+        <input type="checkbox" checked={this.props.todo.completed} />
         {this.props.todo.title}
       </li>
     );
