95c1ad4136e84c09d7b53e3f265920e5d5764c0a
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-03 01:11:19 +0430

    Add style for completed todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,6 +5,12 @@ import styles from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
     return (
       <li className={styles.item}>
         <input
@@ -13,10 +19,15 @@ class TodoItem extends React.Component {
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
-        {this.props.todo.title}
-        <button className={styles.button} onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button
+          className={styles.button}
+          onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+        >
           Delete
         </button>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
       </li>
     );
   }
