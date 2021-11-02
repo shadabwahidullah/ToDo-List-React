2bd5eab48ff84fe60c9ec1c6ddf935666b46e6a9
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-02 23:22:01 +0430

    Change TodoItem from function  based to class based

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function TodoItem (props) {
-    return <li>{props.todo.title}</li>;
+class TodoItem extends React.Component {
+  render() {
+    return <li>{this.props.todo.title}</li>;
+  }
 }
 
 export default TodoItem;
