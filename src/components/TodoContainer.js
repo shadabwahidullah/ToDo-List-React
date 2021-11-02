432a01ea7276ba8b069fe896ea069efc1278e540
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-02 22:55:50 +0430

    Add todos in TodoContainer

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "setup development environment",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Develope website and add content",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      <ul>
+        {this.state.todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
     );
   }
 }
