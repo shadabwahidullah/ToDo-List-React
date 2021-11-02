129113e80c6dba5198c516761879637be837ff60
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-02 23:43:12 +0430

    Pass handle funtion from todo container to it's children todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,11 @@ class TodoItem extends React.Component {
   render() {
     return (
       <li>
-        <input type="checkbox" checked={this.props.todo.completed} />
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps()}
+        />
         {this.props.todo.title}
       </li>
     );
