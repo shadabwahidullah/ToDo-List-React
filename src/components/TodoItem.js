183ca35741cb818acac0ce54be427a6873ed4ce8
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-03 01:03:28 +0430

    Add style for items using Css module

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+//stylesheet
+import styles from "./TodoItem.module.css";
+
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
+          className={styles.checkbox}
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
         {this.props.todo.title}
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button className={styles.button} onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
           Delete
         </button>
       </li>
