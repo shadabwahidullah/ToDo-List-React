4d7847aa5740d5d79495b388bda0e465d55adb3f
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-03 00:47:06 +0430

    Add app.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoContainer from './components/TodoContainer';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoContainer from "./components/TodoContainer";
 
-ReactDOM.render(<TodoContainer/>,document.getElementById("root"));
+import "./App.css";
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"));
