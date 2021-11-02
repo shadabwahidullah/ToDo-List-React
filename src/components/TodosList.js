d6436c740dccc64a645826d4ad223844d9633ef0
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-02 23:14:07 +0430

    Add TodoItem.js component

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-class TodosList extends React.Component {
+import TodoItem from "./TodoItem";
 
+class TodosList extends React.Component {
   render() {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          <li>{todo.title}</li>
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </ul>
     );
