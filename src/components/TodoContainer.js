bcf4bb46c05fe779a5bea3e2196be361c9af079f
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-07 22:08:36 +0430

    Add componentDidUpdate and remove fetching data from API

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -10,10 +10,11 @@ class TodoContainer extends React.Component {
     todos: [],
   };
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then((response) => response.json())
-      .then((data) => this.setState({ todos: data }));
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
   }
 
   handleChange = (id) => {
