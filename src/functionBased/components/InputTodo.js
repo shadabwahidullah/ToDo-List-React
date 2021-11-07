6e6628479baef88e555dcc2f9dabb211fca36cda
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-07 22:59:51 +0430

    Update InputTodo.js component

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
-const InputTodo = props => {
-  const [title, setTitle] = useState("");
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  });
 
-  const onChange = e => {
+  const onChange = (e) => {
     setInputText({
       ...inputText,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (title.trim()) {
-      props.addTodoProps(title);
-      setTitle("");
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title);
+      setInputText({
+        title: "",
+      });
     } else {
       alert("Please write item");
     }
@@ -26,7 +30,7 @@ const InputTodo = props => {
         type="text"
         className="input-text"
         placeholder="Add todo..."
-        value={title}
+        value={inputText.title}
         name="title"
         onChange={onChange}
       />
