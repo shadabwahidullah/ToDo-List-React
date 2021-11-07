9cb201ebc86549ccf54c3795b924e724618e14c3
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-08 00:46:29 +0430

    Resolve linters' errors of TodoList component

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 import TodoItem from './TodoItem';
 
-const TodosList = (props) => (
-  <ul>
-    {props.todos.map((todo) => (
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        handleChangeProps={props.handleChangeProps}
-        deleteTodoProps={props.deleteTodoProps}
-        setUpdate={props.setUpdate}
-      />
-    ))}
-  </ul>
-);
+const TodosList = (props) => {
+  const [todos, handleChangeProps, deleteTodoProps, setUpdate] = props;
+  return (
+    <ul>
+      {todos.map((todo) => {
+        const [id] = todos;
+        return (
+          <TodoItem
+            key={id}
+            todo={todo}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+            setUpdate={setUpdate}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 export default TodosList;
